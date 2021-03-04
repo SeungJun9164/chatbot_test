46ed4ed5841e723aa2acd3ca6b6e5ea42775c886
SeungJun-Lee <lickyjun@naver.com>
2021-03-04 15:23:54 +0900

    Add files via upload

--- KoBART.py
+++ KoBART.py
@@ -7,6 +7,9 @@
 # argparse
 # https://greeksharifa.github.io/references/2019/02/12/argparse-usage/
 
+# __getitem__ , __len__
+# http://hyeonjae-blog.logdown.com/posts/776615-python-getitem-len
+
 # pytorch-lightning
 # https://baeseongsu.github.io/posts/pytorch-lightning-introduction/
 
@@ -45,13 +48,14 @@ class ArgsBase():
     def add_model_specific_args(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
         
-        parser.add_argument('--train_file', type=str, default=r'C:\Users\abc\Desktop\kobart_test\Chatbot_data\train.csv',
+        parser.add_argument('--train_file', type=str, default='Chatbot_data/train.csv',
                             help='train file')
 
-        parser.add_argument('--test_file', type=str, default=r'C:\Users\abc\Desktop\kobart_test\Chatbot_data\test.csv',
+        parser.add_argument('--test_file', type=str, default='Chatbot_data/train.csv',
                             help='test file')
-
-        parser.add_argument('--tokenizer_path', type=str, default=r'C:\Users\abc\jupyter\pytorch\chatbot\emji_tokenizer',
+        
+        # tokenizer_path : emji tokenizer경로 설정
+        parser.add_argument('--tokenizer_path', type=str, default='tokenizer', 
                             help='tokenizer')
         
         parser.add_argument('--batch_size', type=int, default=14,
@@ -63,7 +67,7 @@ class ArgsBase():
         return parser
 
 
-class ChatDataset(Dataset):
+class ChatDataset():
     def __init__(self, filepath, tok_vocab, max_seq_len=128) -> None:
         self.filepath = filepath
         self.data = pd.read_csv(self.filepath)
@@ -71,41 +75,54 @@ class ChatDataset(Dataset):
         self.eos_token = '</s>'
         self.max_seq_len = max_seq_len
         self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=tok_vocab,
-            bos_token=self.bos_token, eos_token=self.eos_token, unk_token='<unk>', pad_token='<pad>', mask_token='<mask>')
+                                                 bos_token=self.bos_token, eos_token=self.eos_token, unk_token='<unk>',
+                                                 pad_token='<pad>', mask_token='<mask>')
 
     def __len__(self):
         return len(self.data)
 
     def make_input_id_mask(self, tokens, index):
+        # input_id : [0, 19471, 15674, 14362, 14877, 21853, 14105, 15339, 245, 1]
         input_id = self.tokenizer.convert_tokens_to_ids(tokens)
         attention_mask = [1] * len(input_id)
-        
+
         if len(input_id) < self.max_seq_len:
             while len(input_id) < self.max_seq_len:
-                input_id += [self.tokenizer.pad_token_id]
+                input_id += [self.tokenizer.pad_token_id]  # 최대 길이보다 짧을 시 pad_token으로 처리
                 attention_mask += [0]
         else:
             # logging.warning(f'exceed max_seq_len for given article : {index}')
             input_id = input_id[:self.max_seq_len - 1] + [
                 self.tokenizer.eos_token_id]
             attention_mask = attention_mask[:self.max_seq_len]
-            
+
         return input_id, attention_mask
 
     def __getitem__(self, index):
+        # recod : 좋아하는 남자 때문에 너무 기분이 안 좋아.,무슨 일이 있었나봐요.,2
         record = self.data.iloc[index]
+
+        # q : 좋아하는 남자 때문에 너무 기분이 안 좋아. / a : 무슨 일이 있었나봐요.
         q, a = record['Q'], record['A']
+
+        # q_tokens : ['<s>', '▁좋아하는', '▁남자', '▁때문에', '▁너무', '▁기분이', '▁안', '▁좋아', '.', '</s>']
+        # a_tokens : ['<s>', '▁무슨', '▁일이', '▁있었', '나', '봐', '요.', '</s>']
         q_tokens = [self.bos_token] + self.tokenizer.tokenize(q) + [self.eos_token]
         a_tokens = [self.bos_token] + self.tokenizer.tokenize(a) + [self.eos_token]
+
+        # encoder_input_id : (0, ..., 1, 3, 3, ...) (0 : bos_token = <s> , 1 : eos_token = </s>, 3 : pad_token)
         encoder_input_id, encoder_attention_mask = self.make_input_id_mask(q_tokens, index)
         decoder_input_id, decoder_attention_mask = self.make_input_id_mask(a_tokens, index)
+
+        # labels : 마스킹 된 언어 모델링 손실을 계산하기위한 레이블
+        # [16046, 15029, 14418,  9495, 10901, 14543, 1, -100, -100, ...]
         labels = self.tokenizer.convert_tokens_to_ids(a_tokens[1:(self.max_seq_len + 1)])
-        
+
         if len(labels) < self.max_seq_len:
             while len(labels) < self.max_seq_len:
                 # for cross entropy loss masking
                 labels += [-100]
-                
+
         return {'input_ids': np.array(encoder_input_id, dtype=np.int_),
                 'attention_mask': np.array(encoder_attention_mask, dtype=np.float),
                 'decoder_input_ids': np.array(decoder_input_id, dtype=np.int_),
